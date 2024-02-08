e20bfe21430baddbc464a4c5c91a5aee2f1d5d5d
Abrham <abrsh6266@gmail.com>
2024-02-08 11:25:11 -0900

    updating the tour component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,49 +1,24 @@
 import React from "react";
 import Title from "./Title";
-
+import { services } from "../data";
 const Services = () => {
   return (
     <section className="section services" id="services">
-      <Title title= 'our' subTitle= 'services' />
+      <Title title="our" subTitle="services" />
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          return (
+            <article className="service" key={service.id}>
+              <span className="service-icon">
+                <i className={service.icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{service.title}</h4>
+                <p className="service-text">{service.text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
     { id: 1, href: '#home', text: 'home' },
     { id: 2, href: '#about', text: 'about' },
@@ -28,5 +33,47 @@ export const pageLinks = [
       icon: 'fas fa-socks fa-fw',
       title: 'amazing comfort',
       text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    },
+  ];
+  export const tours = [
+    {
+      id: 1,
+      image: tour1,
+      date: 'august 26th, 2020',
+      title: 'Tibet Adventure',
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: 'china',
+      duration: 6,
+      cost: 2100,
+    },
+    {
+      id: 2,
+      image: tour2,
+      date: 'october 1th, 2020',
+      title: 'best of java',
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: 'indonesia',
+      duration: 11,
+      cost: 1400,
+    },
+    {
+      id: 3,
+      image: tour3,
+      date: 'september 15th, 2020',
+      title: 'explore hong kong',
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: 'hong kong',
+      duration: 8,
+      cost: 5000,
+    },
+    {
+      id: 4,
+      image: tour4,
+      date: 'december 5th, 2019',
+      title: 'kenya highlights',
+      info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+      location: 'kenya',
+      duration: 20,
+      cost: 3300,
     },
   ];
