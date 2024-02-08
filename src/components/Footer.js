0a50952bc097fb99f58cbff2fa3997244c311735
Abrham <abrsh6266@gmail.com>
2024-02-08 11:51:40 -0900

    some updates with adding page link component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,62 +1,24 @@
 import React from "react";
-
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
 const Footer = () => {
   return (
     <footer className="section footer">
-      <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
-      </ul>
+      <PageLinks  parentClass={'footer-links'} itemClass={'footer-link'}/>
       <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {socialLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} target="_blank" className="footer-icon">
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/logo.svg";
-import { pageLinks, socialLinks } from "../data";
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
 const NavBar = () => {
   return (
     <nav className="navbar">
@@ -11,17 +12,7 @@ const NavBar = () => {
             <i className="fas fa-bars"></i>
           </button>
         </div>
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <PageLinks parentClass={'nav-links'} itemClass={'nav-link'}/>
         <ul className="nav-icons">
           {socialLinks.map((link) => {
             return (
