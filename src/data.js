d47065e996e354dc0e5d81fff2a09cde1389e502
Abrham <abrsh6266@gmail.com>
2024-02-08 04:59:34 -0900

    updating about page and hero

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const pageLinks = [
+    { id: 1, href: '#home', text: 'home' },
+    { id: 2, href: '#about', text: 'about' },
+    { id: 3, href: '#services', text: 'services' },
+    { id: 4, href: '#tours', text: 'tours' },
+  ];
+  export const socialLinks = [
+    { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+    { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+    { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+  ];
+  
+  export const services = [
+    {
+      id: 1,
+      icon: 'fas fa-wallet fa-fw',
+      title: 'saving money',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    },
+    {
+      id: 2,
+      icon: 'fas fa-tree fa-fw',
+      title: 'endless hiking',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    },
+    {
+      id: 3,
+      icon: 'fas fa-socks fa-fw',
+      title: 'amazing comfort',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    },
+  ];
